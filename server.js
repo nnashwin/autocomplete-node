7585feabf0ac5c69dc40531ffd6e9f0917d68363
ttymed <tyler.boright@gmail.com>
2017-05-06 14:33:16 +0800

    adds package to render the index.html file and a route to render the results to show in the autocomplete field

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ var koa = require('koa');
 var fs = require('fs');
 var app = new koa();
 var router = require('koa-router')();
-const serve = require('koa-static');
-
-app.use(serve(__dirname + '/public'));
+var views = require('koa-views');
+const PORT = process.env.PORT || 4000;
 
 var results = [];
 
+
+app.use(views(__dirname + '/public'));
+
+router.get('/', async (ctx) => {
+	await ctx.render('index.html');
+})
+
 router.get('/results', async (ctx) => {
-	this.body = await results;
+	ctx.body = results;
 })
 
 app
 	.use(router.routes());
 
-app.listen(4000);
+console.log(`server is running on port ${PORT}`);
+app.listen(PORT);
