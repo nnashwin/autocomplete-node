edcde1a99cf4bb4504b89f1cd26806346108d87d
ttymed <tyler.boright@gmail.com>
2017-05-06 14:41:23 +0800

    adds a result to the results object

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var router = require('koa-router')();
 var views = require('koa-views');
 const PORT = process.env.PORT || 4000;
 
-var results = [];
+var results = {
+	queries: ['carrot', 'cat', 'carpenter'],
+};
 
 
 app.use(views(__dirname + '/public'));
